aac881449c632a9e02df2848e826c73af77ab23f
Cholavaram <ncholavaram@lm-sjn-21017550.attlocal.net>
2019-03-25 00:55:12 -0700

    Fixed quiz, ideologies pending

--- ideologies.js
+++ ideologies.js
@@ -2,469 +2,573 @@ ideologies = [
     {
         "name": "Anarcho-Communism",
         "stats": {
-            "econ": 100,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 90
+            "econ_asp": 100,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 100,
+            "anarche": 90
         }
     },
     {
         "name": "Libertarian Communism",
         "stats": {
-            "econ": 100,
-            "dipl": 70,
-            "govt": 80,
-            "scty": 80
+            "econ_asp": 100,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 70,
+            "akraetia": 80,
+            "anarche": 80
         }
     },
     {
         "name": "Trotskyism",
         "stats": {
-            "econ": 100,
-            "dipl": 100,
-            "govt": 60,
-            "scty": 80
+            "econ_asp": 100,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 100,
+            "akraetia": 60,
+            "anarche": 80
         }
     },
     {
         "name": "Marxism",
         "stats": {
-            "econ": 100,
-            "dipl": 70,
-            "govt": 40,
-            "scty": 80
+            "econ_asp": 100,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 70,
+            "akraetia": 40,
+            "anarche": 80
         }
     },
     {
         "name": "De Leonism",
         "stats": {
-            "econ": 100,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 80
+            "econ_asp": 100,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 30,
+            "akraetia": 30,
+            "anarche": 80
         }
     },
     {
         "name": "Leninism",
         "stats": {
-            "econ": 100,
-            "dipl": 40,
-            "govt": 20,
-            "scty": 70
+            "econ_asp": 100,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 40,
+            "akraetia": 20,
+            "anarche": 70
         }
     },
     {
         "name": "Stalinism/Maoism",
         "stats": {
-            "econ": 100,
-            "dipl": 20,
-            "govt": 0,
-            "scty": 60
+            "econ_asp": 100,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 20,
+            "akraetia": 0,
+            "anarche": 60
         }
     },
     {
         "name": "Religious Communism",
         "stats": {
-            "econ": 100,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 30
+            "econ_asp": 100,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 30,
+            "anarche": 30
         }
     },
     {
         "name": "State Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 70
+            "econ_asp": 80,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 30,
+            "akraetia": 30,
+            "anarche": 70
         }
     },
     {
         "name": "Theocratic Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 20
+            "econ_asp": 80,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 30,
+            "anarche": 20
         }
     },
     {
         "name": "Religious Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 70,
-            "scty": 20
+            "econ_asp": 80,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 70,
+            "anarche": 20
         }
     },
     {
         "name": "Democratic Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 80
+            "econ_asp": 80,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 50,
+            "anarche": 80
         }
     },
     {
         "name": "Revolutionary Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 20,
-            "govt": 50,
-            "scty": 70
+            "econ_asp": 80,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 20,
+            "akraetia": 50,
+            "anarche": 70
         }
     },
     {
         "name": "Libertarian Socialism",
         "stats": {
-            "econ": 80,
-            "dipl": 80,
-            "govt": 80,
-            "scty": 80
+            "econ_asp": 80,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 80,
+            "akraetia": 80,
+            "anarche": 80
         }
     },
     {
         "name": "Anarcho-Syndicalism",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 80
+            "econ_asp": 80,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 100,
+            "anarche": 80
         }
     },
     {
         "name": "Left-Wing Populism",
         "stats": {
-            "econ": 60,
-            "dipl": 40,
-            "govt": 30,
-            "scty": 70
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 40,
+            "akraetia": 30,
+            "anarche": 70
         }
     },
     {
         "name": "Theocratic Distributism",
         "stats": {
-            "econ": 60,
-            "dipl": 40,
-            "govt": 30,
-            "scty": 20
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 40,
+            "akraetia": 30,
+            "anarche": 20
         }
     },
     {
         "name": "Distributism",
         "stats": {
-            "econ": 60,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 20
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 50,
+            "anarche": 20
         }
     },
     {
         "name": "Social Liberalism",
         "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 80
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 60,
+            "akraetia": 60,
+            "anarche": 80
         }
     },
     {
         "name": "Christian Democracy",
         "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 50,
-            "scty": 30
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 60,
+            "akraetia": 50,
+            "anarche": 30
         }
     },
     {
         "name": "Social Democracy",
         "stats": {
-            "econ": 60,
-            "dipl": 70,
-            "govt": 60,
-            "scty": 80
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 70,
+            "akraetia": 60,
+            "anarche": 80
         }
     },
     {
         "name": "Progressivism",
         "stats": {
-            "econ": 60,
-            "dipl": 80,
-            "govt": 60,
-            "scty": 100
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 80,
+            "akraetia": 60,
+            "anarche": 100
         }
     },
     {
         "name": "Anarcho-Mutualism",
         "stats": {
-            "econ": 60,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 70
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 100,
+            "anarche": 70
         }
     },
     {
         "name": "National Totalitarianism",
         "stats": {
-            "econ": 50,
-            "dipl": 20,
-            "govt": 0,
-            "scty": 50
+            "econ_asp": 50,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 20,
+            "akraetia": 0,
+            "anarche": 50
         }
     },
     {
         "name": "Global Totalitarianism",
         "stats": {
-            "econ": 50,
-            "dipl": 80,
-            "govt": 0,
-            "scty": 50
+            "econ_asp": 50,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 80,
+            "akraetia": 0,
+            "anarche": 50
         }
     },
     {
         "name": "Technocracy",
         "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 20,
-            "scty": 70
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 60,
+            "akraetia": 20,
+            "anarche": 70
         }
     },
     {
         "name": "Centrist",
         "stats": {
-            "econ": 50,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 50
+            "econ_asp": 50,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 50,
+            "anarche": 50
         }
     },
     {
         "name": "Liberalism",
         "stats": {
-            "econ": 50,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 60
+            "econ_asp": 50,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 60,
+            "akraetia": 60,
+            "anarche": 60
         }
     },
     {
         "name": "Religious Anarchism",
         "stats": {
-            "econ": 50,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 20
+            "econ_asp": 50,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 100,
+            "anarche": 20
         }
     },
     {
         "name": "Right-Wing Populism",
         "stats": {
-            "econ": 40,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 30
+            "econ_asp": 40,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 30,
+            "akraetia": 30,
+            "anarche": 30
         }
     },
     {
         "name": "Moderate Conservatism",
         "stats": {
-            "econ": 40,
-            "dipl": 40,
-            "govt": 50,
-            "scty": 30
+            "econ_asp": 40,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 40,
+            "akraetia": 50,
+            "anarche": 30
         }
     },
     {
         "name": "Reactionary",
         "stats": {
-            "econ": 40,
-            "dipl": 40,
-            "govt": 40,
-            "scty": 10
+            "econ_asp": 40,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 40,
+            "akraetia": 40,
+            "anarche": 10
         }
     },
     {
         "name": "Social Libertarianism",
         "stats": {
-            "econ": 60,
-            "dipl": 70,
-            "govt": 80,
-            "scty": 70
+            "econ_asp": 60,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 70,
+            "akraetia": 80,
+            "anarche": 70
         }
     },
     {
         "name": "Libertarianism",
         "stats": {
-            "econ": 40,
-            "dipl": 60,
-            "govt": 80,
-            "scty": 60
+            "econ_asp": 40,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 60,
+            "akraetia": 80,
+            "anarche": 60
         }
     },
     {
         "name": "Anarcho-Egoism",
         "stats": {
-            "econ": 40,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
+            "econ_asp": 40,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 100,
+            "anarche": 50
         }
     },
     {
         "name": "Nazism",
         "stats": {
-            "econ": 40,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 5
+            "econ_asp": 40,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 0,
+            "akraetia": 0,
+            "anarche": 5
         }
     },
     {
         "name": "Autocracy",
         "stats": {
-            "econ": 50,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 50
+            "econ_asp": 50,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 20,
+            "akraetia": 20,
+            "anarche": 50
         }
     },
     {
         "name": "Fascism",
         "stats": {
-            "econ": 40,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 20
+            "econ_asp": 40,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 20,
+            "akraetia": 20,
+            "anarche": 20
         }
     },
     {
         "name": "Capitalist Fascism",
         "stats": {
-            "econ": 20,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 20
+            "econ_asp": 20,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 20,
+            "akraetia": 20,
+            "anarche": 20
         }
     },
     {
         "name": "Conservatism",
         "stats": {
-            "econ": 30,
-            "dipl": 40,
-            "govt": 40,
-            "scty": 20
+            "econ_asp": 30,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 40,
+            "akraetia": 40,
+            "anarche": 20
         }
     },
     {
         "name": "Neo-Liberalism",
         "stats": {
-            "econ": 30,
-            "dipl": 30,
-            "govt": 50,
-            "scty": 60
+            "econ_asp": 30,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 30,
+            "akraetia": 50,
+            "anarche": 60
         }
     },
     {
         "name": "Classical Liberalism",
         "stats": {
-            "econ": 30,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 80
+            "econ_asp": 30,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 60,
+            "akraetia": 60,
+            "anarche": 80
         }
     },
     {
         "name": "Authoritarian Capitalism",
         "stats": {
-            "econ": 20,
-            "dipl": 30,
-            "govt": 20,
-            "scty": 40
+            "econ_asp": 20,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 30,
+            "akraetia": 20,
+            "anarche": 40
         }
     },
     {
         "name": "State Capitalism",
         "stats": {
-            "econ": 20,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 50
+            "econ_asp": 20,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 30,
+            "anarche": 50
         }
     },
     {
         "name": "Neo-Conservatism",
         "stats": {
-            "econ": 20,
-            "dipl": 20,
-            "govt": 40,
-            "scty": 20
+            "econ_asp": 20,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 20,
+            "akraetia": 40,
+            "anarche": 20
         }
     },
     {
         "name": "Fundamentalism",
         "stats": {
-            "econ": 20,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 5
+            "econ_asp": 20,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 30,
+            "akraetia": 30,
+            "anarche": 5
         }
     },
     {
         "name": "Libertarian Capitalism",
         "stats": {
-            "econ": 20,
-            "dipl": 50,
-            "govt": 80,
-            "scty": 60
+            "econ_asp": 20,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 80,
+            "anarche": 60
         }
     },
     {
         "name": "Market Anarchism",
         "stats": {
-            "econ": 20,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
+            "econ_asp": 20,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 100,
+            "anarche": 50
         }
     },
     {
         "name": "Objectivism",
         "stats": {
-            "econ": 10,
-            "dipl": 50,
-            "govt": 90,
-            "scty": 40
+            "econ_asp": 10,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 90,
+            "anarche": 40
         }
     },
     {
         "name": "Totalitarian Capitalism",
         "stats": {
-            "econ": 0,
-            "dipl": 30,
-            "govt": 0,
-            "scty": 50
+            "econ_asp": 0,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 30,
+            "akraetia": 0,
+            "anarche": 50
         }
     },
     {
         "name": "Ultra-Capitalism",
         "stats": {
-            "econ": 0,
-            "dipl": 40,
-            "govt": 50,
-            "scty": 50
+            "econ_asp": 0,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 40,
+            "akraetia": 50,
+            "anarche": 50
         }
     },
     {
         "name": "Anarcho-Capitalism",
         "stats": {
-            "econ": 0,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
+            "econ_asp": 0,
+	    "econ_control": 100,
+	    "nationalism": 100,
+            "cosmopolitanism": 50,
+            "akraetia": 100,
+            "anarche": 50
         }
     }
 ];
